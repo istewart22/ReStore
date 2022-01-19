f9e553d772b3f88ae91727b6096cc76eb0d53bff
Iain Stewart <iainstewart08@gmail.com>
2022-01-19 23:36:24 +0000

    Setup CORS policy

--- API/Startup.cs
+++ API/Startup.cs
@@ -39,6 +39,8 @@ namespace API
             { 
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection")); 
             }); 
+
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +54,10 @@ namespace API
             }
 
             app.UseRouting();
+
+            app.UseCors(opt => {
+                opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+            })
 
             app.UseAuthorization();
 
