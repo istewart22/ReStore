f606f136a3191cd0b3a8fa4a6c4a74330c8d6d8f
Iain Stewart <iainstewart08@gmail.com>
2022-06-15 22:21:11 +0100

    AddBasketEntity migration

--- API/Data/Migrations/20220615211653_AddBasketEntity.Designer.cs
+++ API/Data/Migrations/20220615211653_AddBasketEntity.Designer.cs
@@ -2,14 +2,16 @@
 using API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace API.Data.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    partial class StoreContextModelSnapshot : ModelSnapshot
+    [Migration("20220615211653_AddBasketEntity")]
+    partial class AddBasketEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
