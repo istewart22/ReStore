e55aae02f91bbcdfc06533f3ed12b49010d73cc5
Iain Stewart <iain.stewart@kpmg.co.uk>
2022-06-15 18:08:27 +0100

    Add Basket entity

---
+++ API/Entities/Basket.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace API.Entities
+{
+    public class Basket
+    {
+        public int Id { get; set; }
+        public string BuyerId { get; set; }
+        public List<BasketItem> Items { get; set; } = new();
+
+        public void AddItem(Product product, int quantity) {
+            if (Items.All(item => item.ProductId != product.Id))
+            {
+                Items.Add(new BasketItem{Product = product, Quantity = quantity});
+            }
+
+            var item = Items.FirstOrDefault(item => item.ProductId == product.Id);
+            if (item != null) item.Quantity += quantity;
+        }
+
+        public void RemoveItem(Product product, int quantity) {
+            var item = Items.FirstOrDefault(item => item.ProductId == product.Id);
+            
+            if (item == null) return;
+            item.Quantity -= quantity;
+        }
+    }
+}
